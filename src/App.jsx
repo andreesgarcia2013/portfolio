40e4109b75846a2e60e382a6d8dcbcdf626c6174
andreesgarcia2013 <18030514@itcelaya.edu.mx>
2023-11-14 21:15:31 -0600

    feat/react-route-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 import React, { useEffect, useState } from 'react'
+import { Navbar } from './components/Navbar';
+import { AppRouter } from './router/AppRouter';
 
 export const App = () => {
-  const [isDarkMode, setIsDarkMode] = useState(true);
 
-  const handleChangeTheme = () => {
-    setIsDarkMode(state=>!state);
-  }
-
-  useEffect(() => {
-    if (isDarkMode) {
-      document.querySelector('html').classList.add('dark')
-    }
-    else{
-      document.querySelector('html').classList.remove('dark')
-    }
-  }, [])
   
 
   return (
-    <div className='bg-purple-200 dark:bg-blue-950 w-screen h-screen'>
-      Hola Mundo
-    </div>
+    <>
+      <AppRouter/>
+    </>
   )
 }
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Navbar } from '../components/Navbar'
+
+export const AppRouter = () => {
+  return (
+    <>
+        <Navbar />
+    </>
+  )
+}
